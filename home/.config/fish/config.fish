646f62d5ce7c1c6833d4beec04529d07962a374c
Lucien Müller <lucien.mueller@kontron.com>
2025-08-21 13:30:14 +0200

    config.fish: add fesd function

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -10,3 +10,30 @@ if status is-interactive
         alias lt="lsd $LS_OPTIONS --tree --depth"
     end
 end
+
+# "For Each Subdirectory Do" Function
+function fesd
+    set -l verbose 0
+    if test (count $argv) -eq 0
+        echo "Usage: fesd [-v] <command>"
+        echo "Executes the specified command in each subdirectory of the current directory."
+        return 1
+    end
+    if test "$argv[1]" = "-v"
+        set verbose 1
+        set argv $argv[2..-1]
+    end
+    for dir in (find . -maxdepth 1 -type d)
+        if test "$dir" != "."
+            if test $verbose -eq 1
+                echo "$dir:"
+            end
+            pushd $dir
+            # Execute the command passed to the function
+            if test (count $argv) -gt 0
+                eval $argv
+            end
+            popd
+        end
+    end
+end
